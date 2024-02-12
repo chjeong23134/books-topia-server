7f5f6df0239bf50b4ba501b783b21a4c217b403a
chjeong23134 <chjeong23134@gmail.com>
2024-02-12 11:47:49 +0900

    first commit : 초기 셋업

---
+++ src/main/java/me/bookstopia/BookstopiaApplication.java
@@ -0,0 +1,13 @@
+package me.bookstopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstopiaApplication.class, args);
+	}
+
+}
